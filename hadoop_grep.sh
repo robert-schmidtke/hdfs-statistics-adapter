14250b82d7b3d242ee7ba058695873ed6d846889
Robert Schmidtke <ro.schmidtke@gmail.com>
2016-10-11 14:40:06 +0200

    wrap RawLocalFileSystem during grep job

--- hadoop_grep.sh
+++ hadoop_grep.sh
@@ -4,11 +4,16 @@
 rm -rf /tmp/hadoop*
 rm -rf /tmp/user/$USER
 
-# setup Hadoop
+# setup Hadoop and install the adapter
 cp src/test/resources/hadoop/core-site.xml $HADOOP_HOME/etc/hadoop
 cp src/test/resources/hadoop/hdfs-site.xml $HADOOP_HOME/etc/hadoop
 cp src/test/resources/hadoop/mapred-site.xml $HADOOP_HOME/etc/hadoop
 cp target/hdfs-statistics-adapter-1.0-SNAPSHOT.jar $HADOOP_HOME/share/hadoop/common
+
+# make sure we wrap the local file system
+line=$(grep -n "^    <name>sfs\.wrappedFS\.className<\/name>$" $HADOOP_HOME/etc/hadoop/core-site.xml | cut -d: -f1)
+line=$(($line + 1))
+sed -i "${line}s/.*/    <value>org.apache.hadoop.fs.RawLocalFileSystem<\/value>/" $HADOOP_HOME/etc/hadoop/core-site.xml
 
 # start Hadoop
 $HADOOP_HOME/bin/hdfs namenode -format
