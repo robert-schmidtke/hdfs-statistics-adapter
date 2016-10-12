7937804a374c3079675b038be6f1aeed3f24301a
Robert Schmidtke <ro.schmidtke@gmail.com>
2016-10-12 11:36:45 +0200

    remove old logs, fix input and output files for SFS

--- src/main/scripts/flink-wordcount.sh
+++ src/main/scripts/flink-wordcount.sh
@@ -45,12 +45,13 @@ while [ \$CONNECTED_TASKMANAGERS -lt \${#SLAVES[@]} ]; do
 done
 
 rm -rf $WORK/statistics-fs/output/pg1661.txt
+rm \$FLINK_HOME/log/*
 \$FLINK_HOME/bin/flink run \
   -c org.apache.flink.examples.java.wordcount.WordCount \
   -p \$((\${#SLAVES[@]} * \$TASK_SLOTS)) \
   \$FLINK_HOME/examples/batch/WordCount.jar \
-  --input $WORK/statistics-fs/input/pg1661.txt \
-  --output $WORK/statistics-fs/output/pg1661.txt
+  --input sfs://localhost:8020$WORK/statistics-fs/input/pg1661.txt \
+  --output sfs://localhost:8020$WORK/statistics-fs/output/pg1661.txt
 
 \$FLINK_HOME/bin/stop-cluster.sh
 
