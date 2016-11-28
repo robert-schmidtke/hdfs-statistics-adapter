2b52ee46d65bd8e38360b29b615c1c3314916d35
Robert Schmidtke <ro.schmidtke@gmail.com>
2016-11-28 16:09:39 +0100

    initialize fsLogger for non-ignored files

--- src/main/java/de/zib/sfs/instrument/FileInputStreamAdapter.java
+++ src/main/java/de/zib/sfs/instrument/FileInputStreamAdapter.java
@@ -7,12 +7,18 @@
  */
 package de.zib.sfs.instrument;
 
+import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 
 /**
  * Class adapter that instruments {@link java.io.FileInputStream}.
@@ -60,10 +66,9 @@ public class FileInputStreamAdapter extends ClassVisitor {
             String signature, String[] exceptions) {
         MethodVisitor mv;
         if ("open".equals(name)) {
-            // TODO rename native open method so we can wrap it
-            // mv = cv.visitMethod(access, nativeMethodPrefix + name, desc,
-            // signature, exceptions);
-            mv = cv.visitMethod(access, name, desc, signature, exceptions);
+            // rename native open method so we can wrap it
+            mv = cv.visitMethod(access, nativeMethodPrefix + name, desc,
+                    signature, exceptions);
         } else if ("read".equals(name)) {
             // TODO rename native read method so we can wrap it
             // mv = cv.visitMethod(access, nativeMethodPrefix + name, desc,
@@ -88,6 +93,56 @@ public class FileInputStreamAdapter extends ClassVisitor {
 
     @Override
     public void visitEnd() {
+        // private Logger fsLogger;
+        FieldVisitor fsLoggerFV = cv.visitField(Opcodes.ACC_PRIVATE,
+                "fsLogger", Type.getDescriptor(Logger.class), null, null);
+        fsLoggerFV.visitEnd();
+
+        // private void open(String name) {
+        MethodVisitor openMV = cv.visitMethod(Opcodes.ACC_PRIVATE, "open",
+                methodDescriptors.get("open"), methodSignatures.get("open"),
+                methodExceptions.get("open"));
+        openMV.visitCode();
+
+        // if (!ignoreFileName.equals(name)) {
+        openMV.visitLdcInsn(ignoreFileName);
+        openMV.visitVarInsn(Opcodes.ALOAD, 1);
+        openMV.visitMethodInsn(
+                Opcodes.INVOKEVIRTUAL,
+                Type.getInternalName(String.class),
+                "equals",
+                Type.getMethodDescriptor(Type.BOOLEAN_TYPE,
+                        Type.getType(Object.class)), false);
+        Label ignoreFileNameEqualsName = new Label();
+        openMV.visitJumpInsn(Opcodes.IFNE, ignoreFileNameEqualsName);
+
+        // fsLogger = LogManager.getLogger("de.zib.sfs.AsyncLogger");
+        openMV.visitVarInsn(Opcodes.ALOAD, 0);
+        openMV.visitLdcInsn("de.zib.sfs.AsyncLogger");
+        openMV.visitMethodInsn(
+                Opcodes.INVOKESTATIC,
+                Type.getInternalName(LogManager.class),
+                "getLogger",
+                Type.getMethodDescriptor(Type.getType(Logger.class),
+                        Type.getType(String.class)), false);
+        openMV.visitFieldInsn(Opcodes.PUTFIELD,
+                Type.getInternalName(FileInputStream.class), "fsLogger",
+                Type.getDescriptor(Logger.class));
+
+        // }
+        openMV.visitLabel(ignoreFileNameEqualsName);
+
+        // nativeMethodPrefixopen(name);
+        openMV.visitVarInsn(Opcodes.ALOAD, 0);
+        openMV.visitVarInsn(Opcodes.ALOAD, 1);
+        openMV.visitMethodInsn(Opcodes.INVOKESPECIAL,
+                Type.getInternalName(FileInputStream.class), nativeMethodPrefix
+                        + "open", methodDescriptors.get("open"), false);
+
+        // }
+        openMV.visitInsn(Opcodes.RETURN);
+        openMV.visitMaxs(0, 0);
+        openMV.visitEnd();
 
         cv.visitEnd();
 
