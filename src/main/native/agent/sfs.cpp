7acd859f79aa6c1246b8e059ea221464fa70d884
Robert Schmidtke <ro.schmidtke@gmail.com>
2016-12-12 13:59:33 +0100

    init random with nanoseconds

--- src/main/native/agent/sfs.cpp
+++ src/main/native/agent/sfs.cpp
@@ -133,7 +133,9 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *vm, char *options, void *reserved) {
 
   // assume that all that can go wrong during startup is a port that is already
   // in use
-  srand(time(NULL));
+  timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  srand((time_t)ts.tv_nsec);
 
   int port, tries = 0;
   bool started = false;
