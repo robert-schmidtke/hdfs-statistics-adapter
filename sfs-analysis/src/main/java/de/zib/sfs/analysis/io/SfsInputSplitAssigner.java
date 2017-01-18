9c53813d57d4b8387e132b1c564d12bde5cbfd7f
Robert Schmidtke <ro.schmidtke@gmail.com>
2017-01-18 17:59:47 +0100

    debug logging during input split assignment

--- sfs-analysis/src/main/java/de/zib/sfs/analysis/io/SfsInputSplitAssigner.java
+++ sfs-analysis/src/main/java/de/zib/sfs/analysis/io/SfsInputSplitAssigner.java
@@ -13,14 +13,27 @@ import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 import org.apache.flink.core.io.InputSplitAssigner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SfsInputSplitAssigner implements InputSplitAssigner {
+
+    private static final Logger LOG = LoggerFactory
+            .getLogger(SfsInputSplitAssigner.class);
 
     private final Map<String, ConcurrentLinkedDeque<SfsInputSplit>> unassignedInputSplits;
 
     public SfsInputSplitAssigner(SfsInputSplit[] inputSplits) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Assigning {} input splits", inputSplits.length);
+        }
+
         unassignedInputSplits = new HashMap<>();
         for (SfsInputSplit inputSplit : inputSplits) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Assigning input split {} to host {}",
+                        inputSplit.getSplitNumber(), inputSplit.getHost());
+            }
             unassignedInputSplits.computeIfAbsent(inputSplit.getHost(),
                     host -> new ConcurrentLinkedDeque<>()).push(inputSplit);
         }
@@ -29,6 +42,10 @@ public class SfsInputSplitAssigner implements InputSplitAssigner {
     @Override
     public SfsInputSplit getNextInputSplit(String host, int taskId) {
         try {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Getting next input split for host {} and task {}",
+                        host, taskId);
+            }
             return unassignedInputSplits.get(host).pop();
         } catch (NoSuchElementException e) {
             return null;
