4c6adb34814b4c2de130780d4b52d53ac0e3f694
Robert Schmidtke <ro.schmidtke@gmail.com>
2017-01-23 12:27:27 +0100

    use blocking shutdown on current context

---
+++ sfs-agent/src/main/java/de/zib/sfs/instrument/util/Log4j2BlockingShutdownUtil.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2016 by Robert Schmidtke,
+ *               Zuse Institute Berlin
+ *
+ * Licensed under the BSD License, see LICENSE file for details.
+ *
+ */
+package de.zib.sfs.instrument.util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+
+public class Log4j2BlockingShutdownUtil {
+
+    /**
+     * Log4j2 2.7 uses executor services to execute asynchronous actions (like
+     * compression). The default shutdown does not wait for submitted threads to
+     * complete, possibly resulting in invalid gz archives, because compression
+     * did not finish. Re-implement the
+     * {@link org.apache.logging.log4j.LogManager#shutdown()} here, except wait
+     * quite some time.
+     * 
+     * @param verbose
+     * 
+     * @see <a
+     *      href="https://logging.apache.org/log4j/log4j-2.7/log4j-api/xref/org/apache/logging/log4j/LogManager.html">LogManager</a>
+     * @see <a
+     *      href="https://logging.apache.org/log4j/log4j-2.7/log4j-core/xref/org/apache/logging/log4j/core/LoggerContext.html">LoggerContext</a>
+     * @see <a
+     *      href="https://logging.apache.org/log4j/log4j-2.7/log4j-core/xref/org/apache/logging/log4j/core/util/ExecutorServices.html">ExecutorServices</a>
+     * @see <a
+     *      href="https://logging.apache.org/log4j/log4j-2.7/log4j-core/xref/org/apache/logging/log4j/core/appender/rolling/RollingFileManager.html">RollingFileManager</a>
+     */
+    public static void shutdownLog4j27(boolean verbose) {
+        try {
+            LogUtil.enableStderrLogging(verbose);
+
+            // get current context
+            org.apache.logging.log4j.spi.LoggerContext loggerContext = LogManager
+                    .getContext(false);
+            if (loggerContext != null && loggerContext instanceof LoggerContext) {
+                // close() calls stop() as well, except with default arguments
+                @SuppressWarnings("resource")
+                LoggerContext lc = (LoggerContext) loggerContext;
+
+                LogUtil.stderr("Stopping logger context '%s'.\n", lc.toString());
+
+                long startTime = System.currentTimeMillis();
+                if (lc.stop(600, TimeUnit.SECONDS)) {
+                    LogUtil.stderr("Logger '%s' stopped after %d seconds.\n",
+                            (System.currentTimeMillis() - startTime) / 1000L);
+                } else {
+                    LogUtil.stderr(
+                            "Stopping Logger '%s' timed out after %d seconds.\n",
+                            (System.currentTimeMillis() - startTime) / 1000L);
+                }
+            } else {
+                LogUtil.stderr("Logger context '%s' cannot be stopped.\n",
+                        loggerContext.toString());
+            }
+        } catch (Exception e) {
+            LogUtil.stderr("Error shutting down Log4j2: %s.\n", e.getMessage());
+        }
+    }
+}
