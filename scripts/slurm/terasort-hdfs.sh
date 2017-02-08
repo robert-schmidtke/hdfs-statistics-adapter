bc73eae1ed8e1c453a12c5f3dad97b7952893e2d
Robert Schmidtke <ro.schmidtke@gmail.com>
2017-02-08 11:13:19 +0100

    add template for running collectl

--- scripts/slurm/terasort-hdfs.sh
+++ scripts/slurm/terasort-hdfs.sh
@@ -92,7 +92,20 @@ echo "$(date): Creating local folders"
 srun -N$SLURM_JOB_NUM_NODES mkdir -p /local/$USER/hdfs
 srun -N$SLURM_JOB_NUM_NODES mkdir -p /tmp/sfs
 srun -N$SLURM_JOB_NUM_NODES mkdir -p /local/$USER/flink
+#srun -N$SLURM_JOB_NUM_NODES mkdir -p /local/$USER/collectl
 echo "$(date): Creating local folders done"
+
+#echo "$(date): Starting collectl"
+#start_collectl_script="${SLURM_JOB_ID}-start-collectl.sh"
+#cat >> $start_collectl_script << EOF
+##!/bin/bash
+#nohup collectl -P -f /local/$USER/collectl/\$(hostname).log -s cCdDmMnNZ > /local/$USER/collectl/collectl.log 2>&1 &
+#echo \$! > /local/$USER/collectl/collectl.pid
+#EOF
+#chmod +x $start_collectl_script
+#srun -N$SLURM_JOB_NUM_NODES $start_collectl_script
+#rm $start_collectl_script
+#echo "$(date): Starting collectl done"
 
 if [ -z "$NO_SFS" ]; then
   echo "$(date): Starting transformer JVMs"
@@ -277,6 +290,20 @@ EOF
   echo "$(date): Stopping transformer JVMs done"
 fi
 
+#echo "$(date): Stopping collectl"
+#stop_collectl_script="${SLURM_JOB_ID}-stop-collectl.sh"
+#cat >> $stop_collectl_script << EOF
+##!/bin/bash
+#kill \$(</local/$USER/collectl/collectl.pid)
+#rm /local/$USER/collectl/collectl.pid
+#rm /local/$USER/collectl/collectl.log
+#cp /local/$USER/collectl/\$(hostname).log* /scratch/$USER/\$(hostname).collectl.gz
+#EOF
+#chmod +x $stop_collectl_script
+#srun $stop_collectl_script
+#rm $stop_collectl_script
+#echo "$(date): Stopping collectl done"
+
 echo "$(date): Cleaning Java processes"
 srun -N$SLURM_JOB_NUM_NODES killall -sSIGKILL java
 echo "$(date): Cleaning Java processes done"
@@ -302,6 +329,7 @@ if [ "$RET_CODE" -eq "0" ]; then
   srun -N$SLURM_JOB_NUM_NODES rm -rf /tmp/sfs
   srun -N$SLURM_JOB_NUM_NODES rm -rf /local/$USER/flink
   srun -N$SLURM_JOB_NUM_NODES rm -rf /tmp/*
+#  srun -N$SLURM_JOB_NUM_NODES rm -rf /local/$USER/collectl
   echo "$(date): Cleaning local directories done"
 else
   echo "$(date): Some task did not run successfully, not cleaning local directories."
